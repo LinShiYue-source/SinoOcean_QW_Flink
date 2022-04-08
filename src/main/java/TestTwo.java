30a06cf131382865defd7513bf2d9faa9da69640
LinShiYue-source <zhuyueming099@163.com>
2022-04-08 09:39:06 +0800

    Git 测试3  提交Java文件

---
+++ src/main/java/TestTwo.java
@@ -0,0 +1,10 @@
+/**
+ * User : LinShiYue
+ * Date : 2022-04-08 09:38:40
+ * Description :
+ */
+public class TestTwo {
+    public static void main(String[] args) {
+        System.out.println("test two");
+    }
+}
