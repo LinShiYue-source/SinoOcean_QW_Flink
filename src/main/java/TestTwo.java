8fddfdf124fb4a94a6d8dfd321350a75b0502b64
LinShiYue-source <zhuyueming099@163.com>
2022-04-08 09:43:32 +0800

    Git 测试3  修改Java文件

--- src/main/java/TestTwo.java
+++ src/main/java/TestTwo.java
@@ -1,10 +1,11 @@
 /**
- * User : LinShiYue
  * Date : 2022-04-08 09:38:40
  * Description :
  */
 public class TestTwo {
     public static void main(String[] args) {
         System.out.println("test two");
+        System.out.println("test two");
+
     }
 }
